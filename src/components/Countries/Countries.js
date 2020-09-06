c805e4e8d24b0826cc458fd2e24c8c1ea16e2251
Ahmed-Eldawoody <60572865+Ahmed-Eldawoody@users.noreply.github.com>
2020-09-06 21:21:30 +0200

    ..

--- src/components/Countries/Countries.js
+++ src/components/Countries/Countries.js
@@ -1,47 +1,36 @@
-import React, { useState, useEffect } from "react";
+import React, { useContext } from "react";
+import { Context } from "../../Context/Context";
+import { GiEarthAmerica } from "react-icons/gi";
 import "./Countries.css";
 
 export default function Countries(props) {
-  const [countries, setCountries] = useState([]);
-
-  useEffect(() => {
-    const url =
-      "https://restcountries.eu/rest/v2/all?fields=name;topLevelDomain;capital;region;subregion;population;borders;currencies;languages;flag;nativeName";
-
-    fetch(url)
-      .then(
-        (response) => {
-          if (response.ok) {
-            return response.json();
-          }
-          throw new Error("Requist failed!");
-        },
-        (networkError) => console.log(networkError.message)
-      )
-      .then((jsonResponse) => {
-        setCountries(jsonResponse);
-      });
-  }, []);
+  const { countries, theme } = useContext(Context);
 
   return (
     <div className="countries-1">
-      {countries.map((country) => {
-        return (
-          <div className={`container-${props.theme}`} key={country.name}>
-            <img src={country.flag} alt="country flag" className="flag" />
-            <h3 className="name">{country.name}</h3>
-            <h5 className="population">
-              Population: <span>{country.population.toLocaleString()}</span>
-            </h5>
-            <h5 className="region">
-              Region: <span>{country.region}</span>
-            </h5>
-            <h5 className="capital">
-              capital: <span>{country.capital}</span>
-            </h5>
-          </div>
-        );
-      })}
+      {countries && countries.length > 0 ? (
+        countries.map((country) => {
+          return (
+            <div className={`container-${theme}`} key={country.name}>
+              <img src={country.flag} alt="country flag" className="flag" />
+              <h3 className="name">{country.name}</h3>
+              <h5 className="population">
+                Population: <span>{country.population.toLocaleString()}</span>
+              </h5>
+              <h5 className="region">
+                Region: <span>{country.region}</span>
+              </h5>
+              <h5 className="capital">
+                capital: <span>{country.capital}</span>
+              </h5>
+            </div>
+          );
+        })
+      ) : (
+        <div>
+          <GiEarthAmerica />
+        </div>
+      )}
     </div>
   );
 }
